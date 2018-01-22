bedb3d9b893b49fa13576d5b3e09c485ed20d8bd
Can Avcı <canavci2016@gmail.com>
2018-01-22 23:10:33 +0300

    fix

--- 101-okey-react/src/reducers/reducer_user.js
+++ 101-okey-react/src/reducers/reducer_user.js
@@ -1,35 +1,33 @@
-import  {SIGNED_USER,CLEAR_SIGNED_USER} from  '../constants/constants';
-import { bake_cookie, read_cookie, delete_cookie } from 'sfcookies';
+import {SIGNED_USER, CLEAR_SIGNED_USER} from '../constants/constants';
+import {bake_cookie, read_cookie, delete_cookie} from 'sfcookies';
 
 let user = {
     token: null
 };
 
-export  default (state = user, action)=> {
+export default (state = user, action) => {
 
-    const cookie_name='auth_user';
+    const cookie_name = 'auth_user';
 
     var user_cookie = read_cookie(cookie_name, null);
     if (Array.isArray(user_cookie) || user_cookie == null) {
 
     }
-    else
-    {
-        state=user_cookie;
+    else {
+        state = user_cookie;
     }
 
     switch (action.type) {
         case SIGNED_USER:
-            const {userObject}=action;
+            const {userObject} = action;
             bake_cookie(cookie_name, userObject);
             return userObject;
 
         case CLEAR_SIGNED_USER:
-        delete_cookie(cookie_name);
+            delete_cookie(cookie_name);
             return {
                 token: null
             };
-
         default:
             return state;
 
